6f0ee6a984fb867d4ef064008be30ae548cd75a3
Yu Jeongmin <jeongmin96@postech.ac.kr>
2016-10-07 03:08:56 +0900

    Overwatch Font & login page base

--- src/components/App.js
+++ src/components/App.js
@@ -2,9 +2,14 @@ import React from 'react';
 
 class App extends React.Component {
     render(){
-
         return (
-                <h1>Hello Sytronik&apos;s Page</h1>
+            <div>
+                <h1>OVERWATCH</h1>
+                <h4>로그인</h4>
+                ID : <input type="text" name="ID" value=""/><br/>
+                PW : <input type="password" name="PW" value=""/><br/>
+                <input type="submit" value="Submit"/>
+            </div>
         );
     }
 }
