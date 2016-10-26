fd256eda5944057b9ae195eb58e1c7e0d1912478
Yu Jeongmin <jeongmin96@postech.ac.kr>
2016-10-26 21:54:01 +0900

    edit layout of login page

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -24,17 +24,19 @@ class Background extends React.Component {
 
 class Login extends React.Component {
     render(){
-        var fonts={color:'white', 'font-style':'italic'};
+        var fonts={'color':'white', 'font-style':'italic', 'font-size':'20'};
+        var style_left={'width':'30%', 'margin-left':'4%', 'margin-right':'10px', 'float':'left','text-align':'right'};
+        var style_input={'width':'40%', 'margin':'0','float':'left'};
+        this.state= {codename: "", password:""};
         return (
             <div className={styles.login}>
                 <h3 style={fonts}>로그인</h3>
                 <div style={fonts}>
-                    Codename : <input type="text" name="ID" value=""/><br/>
+                    <div style={style_left}>Codename :</div><input type="text" name="ID" value={this.state.codename} style={style_input}/><br/><br/>
+                    <div style={style_left}>PW :      </div><input type="password" name="PW" value={this.state.password} style={style_input}/><br/>
                 </div>
-                <div style={fonts}>
-                    <br/>PW : <input type="password" name="PW" value=""/><br/>
-                </div>
-                <input type="button" value="Login"/>
+                <br/>
+                <div style={{'width':'70px','height':'20px','font-style':'italic','font-size':'20','background-color':'#ffcc00','margin':'auto','border-radius': '5px','padding-top':'2px'}}>Login</div>
             </div>
         );
     }
