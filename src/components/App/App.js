aafc57a141b42d396d1ff6443e937a5c61bb5012
Yu Jeongmin <jeongmin96@postech.ac.kr>
2016-11-03 23:37:47 +0900

    CRLF & Login Page 보강

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -24,19 +24,24 @@ class Background extends React.Component {
 
 class Login extends React.Component {
     render(){
-        var fonts={'color':'white', 'font-style':'italic', 'font-size':'20'};
-        var style_left={'width':'30%', 'margin-left':'4%', 'margin-right':'10px', 'float':'left','text-align':'right'};
+        var style_left={'width':'30%', 'margin-left':'4%', 'margin-right':'10px', 'float':'left','text-align':'right','padding-top':'2px'};
         var style_input={'width':'40%', 'margin':'0','float':'left'};
-        this.state= {codename: "", password:""};
+        function clickLogin() {
+            if(codename.value=="겐지") {
+                alert("asdf");
+            }
+        }
         return (
             <div className={styles.login}>
-                <h3 style={fonts}>로그인</h3>
-                <div style={fonts}>
-                    <div style={style_left}>Codename :</div><input type="text" name="ID" value={this.state.codename} style={style_input}/><br/><br/>
-                    <div style={style_left}>PW :      </div><input type="password" name="PW" value={this.state.password} style={style_input}/><br/>
-                </div>
-                <br/>
-                <div style={{'width':'70px','height':'20px','font-style':'italic','font-size':'20','background-color':'#ffcc00','margin':'auto','border-radius': '5px','padding-top':'2px'}}>Login</div>
+                <h3>로그인</h3>
+                <form name="frmLogin">
+                    <label for="codename" style={style_left}>CODENAME:</label>
+                    <input type="text" id="codename" name="codename" onChange={function() {}}  className ={styles.input} style={style_input}></input><br/><br/>
+                    <label for="password" style={style_left}>PW :     </label>
+                    <input type="password" id="password" name="password" onChange={function() {}} className ={styles.input} style={style_input}></input><br/>
+                    <br/>
+                    <button className={styles.button} type="submit" onClick={clickLogin}>Login</button>
+                </form>
             </div>
         );
     }
